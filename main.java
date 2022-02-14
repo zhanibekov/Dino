ccc471a97a880858a2c5302d43082c0dd59dccd0
yelaman <67456840+zhanibekov@users.noreply.github.com>
2022-02-14 21:35:22 +0600

    Dino
    Dino

---
+++ main.java
@@ -0,0 +1,5 @@
+public class main {
+    public static void Main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ swift.js
@@ -0,0 +1,33 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus")
+function jump() {
+    if (dino.classList != "jump"){
+    dino.classList.add("jump");
+    
+    
+    setTimeout(function () {
+        dino.classList.remove("jump");
+      }, 300);
+    }
+ }
+
+ let isAlive = setInterval(function() {
+
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+   );
+    console.log(cactusLeft);
+
+
+    if(cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
+      alert("Game Over!");
+    }
+
+
+}, 10);
+
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});  
